196a97c1946743c2ebaeef01c9e69b1c11fec49c
jinxing <jin_xing@aol.com>
2016-11-22 02:01:21 +0800

    Fixed bug in account file

--- controllers/account.php
+++ controllers/account.php
@@ -15,7 +15,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 use App\Helpers\RandomString;
-
+use App\Helpers\GLCache;
 use App\QB;
 
 define('INIT_COIN', 60);
