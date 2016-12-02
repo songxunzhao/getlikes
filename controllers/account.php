1396fc9dc08d28baada588fb0802387e79f5d4fc
jinxing <jin_xing@aol.com>
2016-12-03 00:17:20 +0800

    Added middleware

--- controllers/account.php
+++ controllers/account.php
@@ -85,8 +85,6 @@ class Account {
             $res_data = [
                 'success'   => true,
                 'token'     => $token,
-                'ip'        => $_SERVER['REMOTE_ADDR'],
-                'location'  => $country_code,
                 'data'      => [
                     'user' => [
                         'id'            => $user->id,
--- public/index.php
+++ public/index.php
@@ -60,6 +60,10 @@ $app->post ( '/account/register', 'App\Controller\Account::register');
 $app->get ( '/misc/adcolony', 'App\Controller\Misc::adcolony' );
 $app->get ( '/misc/server_test', 'App\Controller\Misc::server_test' );
 
+$checkProxyHeaders = true; // Note: Never trust the IP address for security processes!
+$trustedProxies = ['10.0.0.1', '10.0.0.2']; // Note: Never trust the IP address for security processes!
+$app->add(new RKA\Middleware\IpAddress($checkProxyHeaders, $trustedProxies));
+
 // Authentication group
 $app->group('',  function () use($app) {
     $app->post ( '/account/add_coin',           'App\Controller\Account::add_coin'          );
